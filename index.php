3cb4550546fd25864c2a7939393567c483bda463
Deniel Stájner <stajner@spsejecna.cz>
2023-03-03 11:15:49 +0100

    2

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ $thing = $today."T".$time_now;
 <html lang="en">
 <head>
     <title>Sunny innit</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
 <div style="text-align: center;">
